8aaffd1349ee0d2e463c6f33eea68c9b49a73059
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-23 22:31:15 +0300

    2 задача

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,18 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
-//---------------
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-//-----------------------
-player.on('timeupdate', throttle(findCurrentTime, 1000))
+player.on('timeupdate', throttle(findCurrentTime, 1000));
 
 function findCurrentTime(data) {
     const videoCurrentTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', videoCurrentTime)  
-};
+    localStorage.setItem('videoplayer-current-time', videoCurrentTime);  
+}
  
-//---------------------
-
 const getCurrantTime = localStorage.getItem('videoplayer-current-time');
 
-//----------------------------
 player
     .setCurrentTime(getCurrantTime)
     .then(function (seconds) { })
