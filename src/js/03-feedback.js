19edb8fef5d99cc2a4c6fcd3f961a9fcb3c4a8f9
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-25 00:18:08 +0300

    3 зд

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const emailElement = document.querySelector('input');
+const messageElement = document.querySelector('textarea');
+const formData = {};
+
+checkLocaleStorage();
+
+emailElement.addEventListener('input', throttle(findInputValues, 500));
+messageElement.addEventListener('input', throttle(findInputValues, 500));
+
+function findInputValues(evt) {
+    evt.preventDefault();
+
+    const name = event.target.name;
+    const value = event.target.value;
+  
+    formData[name] = value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+  
+//----------------
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    
+    const email = event.target.elements.email.value;
+    const message = event.target.elements.message.value;
+    
+    if (email === '' || message === '') {
+        alert('Пожалуйста, заполните все поля!')
+    } else {
+    localStorage.removeItem('feedback-form-state');   
+    console.log({ email, message })
+}
+    event.currentTarget.reset()
+}
+form.addEventListener("submit", onFormSubmit);
+
+//--------------------
+function checkLocaleStorage() {
+    const storageData = localStorage.getItem('feedback-form-state')
+        ? JSON.parse(localStorage.getItem('feedback-form-state'))
+        : {};
+    if (storageData.email) {
+        emailElement.value = storageData.email;
+    }
+    if (storageData.message) {
+        messageElement.value = storageData.message;
+    }
+}
+ 
