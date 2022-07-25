4fd35c9c0beb983a060c233a5e2b80ee08a711e4
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-25 23:33:38 +0300

    hfg

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,20 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-
 const emailElement = document.querySelector('input');
 const messageElement = document.querySelector('textarea');
-const formData = {};
 
-checkLocaleStorage();
+checkingLocaleStorage();
 
-emailElement.addEventListener('input', throttle(findInputValues, 500));
-messageElement.addEventListener('input', throttle(findInputValues, 500));
+form.addEventListener('input', throttle(findInputValues, 500));
 
 function findInputValues(event) {
     event.preventDefault();
 
-    const name = event.target.name;
-    const value = event.target.value;
-  
-    formData[name] = value;
+    let formData = localStorage.getItem('feedback-form-state')
+        ? JSON.parse(localStorage.getItem('feedback-form-state'))
+        : {};
+    formData[event.target.name] = event.target.value;
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
@@ -39,10 +36,8 @@ const onFormSubmit = event => {
 form.addEventListener("submit", onFormSubmit);
 
 //--------------------
-function checkLocaleStorage() {
-    const storageData = localStorage.getItem('feedback-form-state')
-        ? JSON.parse(localStorage.getItem('feedback-form-state'))
-        : {};
+function checkingLocaleStorage() {
+   
     if (storageData.email) {
         emailElement.value = storageData.email;
     }
